8662330078858cb675870b61166b14d0bc6728a3
Raul Cesar <raulcesar.rcf@gmail.com>
2022-08-09 20:31:21 -0300

    Atividade Labenu

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,14 @@
-const num = //seu numero aqui
+const num = Number(prompt("Digite o seu numero "))
 
+if(num%2===0){
+    if(num%3===0){
+        console.log("O numero é divido por 2 e por 3 ")
+
+    }
+    else{console.log("O numero eh dividido por 2 mas nao por 3")}
+}else{console.log("O numero não é divido por 2")}
+
+
+if(num%2===0 && num%3===0){
+    console.log("O numero é divido por 2 e por 3 parabens")
+}else{console.log("O numero não é didido por 2 e por 3 que pena")}
